f29652f80f5babeda2167bd0573c002f8f12741c
kayla-noelle <knchavez14@gmail.com>
2026-02-03 22:11:36 -0800

    installed tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+
+import React, { useState } from "react";
+import { ColorInput } from "./components/ColorInput";
+import Header from "./components/Header";
+
 function App() {
-  return <h1>Hello React!</h1>;
-}
+
+      const [color, setColor] = useState("#4f46e5");
+      const [usage, setUsage ] = useState("body");
+
+  return (
+    <div className="font-bold min-h-screen bg-gray-50 text-gray-900">
+     {/*Header */}
+     <Header />
+
+       {/* Main content */}
+
+       <main className="max-w-4xl mx-auto px-4 py-10 space-y-8">
+        <ColorInput label="Base Color" value={color} onChange={setColor} />
+       </main>
+    </div>   
+
+  );
+};
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header(){
+    return (
+        <header className="bg-white border-b shadow-sm">
+            <div className="max-w-4xl mx-auto px-4 py-6">
+                <h1 className="text-2xl font-semibold tracking-tight text-gray-900">Design Accessibility Tool</h1>
+                <p className="mt-1 text-sm text-gray-500" > WCAG Contrast checks for designers</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
